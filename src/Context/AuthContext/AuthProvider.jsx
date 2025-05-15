dea7d37ecc223f32def51fe3f2a56cf0ebc21a05
Abdullah Al Ashik <abdullahashik17@gmail.com>
2025-05-16 00:34:25 +0600

    implement login and log out in aith provider

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthCOntext';
 import auth from '../../firebase/firebase.config';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import axios from 'axios';
 
 
 const googleProvider = new GoogleAuthProvider()
@@ -34,8 +35,26 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unSubscriber = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
-            console.log('state captured', currentUser);
-            setLoading(false)
+            console.log('state captured', currentUser?.email);
+
+            if(currentUser?.email){
+                const user  = {email : currentUser.email}
+
+                axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
+                .then(res=> {
+                     console.log('login', res.data);
+                     setLoading(false)
+                })
+            }
+            else{
+                axios.post('http://localhost:5000/logout',{},{withCredentials:true})
+                .then(res=>{
+                    console.log('logout', res.data);
+                    setLoading(false)
+                })
+            }
+
+            
         })
         return ()=>{
             unSubscriber()
